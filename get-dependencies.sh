85e1410dc0c920e5b2f3bdca4bd8b6ff51ab47d2
Link4Electronics <link4electronics@gmail.com>
2026-08-16 11:00:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm jre-openjdk openssl
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+VERSION=1.0
+echo "$VERSION" > ~/version
+wget https://downloaditr.receita.fazenda.gov.br/2026/arquivos/${VERSION}/ITR2025v${VERSION}.zip
+bsdtar -xvf ITR2026v${VERSION}.zip --strip-components=1
+rm -f *.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+sed -i 's|\(\./jre/bin/\)\?java -jar pgditr.jar|java -jar "$APPDIR/bin/pgditr.jar" "$@"|g' exec.sh
+mv -v exec.sh lib pgditr.jar RFB.png pgd-updater.jar ./AppDir/bin
