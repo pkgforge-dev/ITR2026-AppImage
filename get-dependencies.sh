900f3e45a3b6e70a913439e6c22227d0ae987ada
Link4Electronics <link4electronics@gmail.com>
2026-09-02 18:26:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm jre-openjdk openssl
+pacman -Syu --noconfirm jre11-openjdk openssl
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
