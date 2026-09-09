cda82d02b5f7991f0fbd62420f51e22179cc3f22
Link4Electronics <link4electronics@gmail.com>
2026-09-09 00:59:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,7 @@ echo "Getting app..."
 echo "---------------------------------------------------------------"
 VERSION=1.1
 echo "$VERSION" > ~/version
-wget https://api.rv.pkgforge.dev/https://downloaditr.receita.fazenda.gov.br/2026/arquivos/${VERSION}/ITR2025v${VERSION}.zip
+wget https://downloaditr.receita.fazenda.gov.br/2026/arquivos/${VERSION}/ITR2025v${VERSION}.zip
 bsdtar -xvf ./ITR2026v${VERSION}.zip --strip-components=1
 rm -f *.zip
 
